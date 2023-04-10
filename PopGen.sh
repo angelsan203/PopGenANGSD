3b4628c394b365087831d043e8ca679b75668b6a
angelsan203 <106264605+angelsan203@users.noreply.github.com>
2023-04-10 11:28:55 -0600

    Update PopGen.sh

--- PopGen.sh
+++ PopGen.sh
@@ -1,8 +1,29 @@
-#!/bin/bash
+#!/bin/bash 
 
-####Burrows Willer Alignment####
+#### Análisis con ANGSD #### 
+### Alelle frequencies ###
 
-bwa mem GenomRef.fasta Genom1.1.fasta Genom1.2.fasta -o Genom.1.sam
+./angsd -b lista1 -anc GenomRef.fasta -out pop1fst -dosaf 1 -GL 1 
+
+# Se hace una vez por cada lista de población "lista 1, lista 2, lista 3 ..." # 
+
+## Si se toman en cuenta dos poblaciones se calculan 2d sfs ##
+
+./angsd/misc/realSFS pop1fst.saf.idx pop2.saf.idx - sfs pop1.pop2.ml -fstout pop1_2
+
+### Estimados globales ###
+
+./angsd -bam_sorted.filelist -GL 1 -doMajorMinor 1 - doMaf 2 -P6 -minMapQ 30 -minQ 20 min.maf 0.05
+
+### Beagle Likelihood ### 
+
+./angsd -GL 1 out Genom1GL -nthreads 6 - do GLF 2 -doMajorminor 1 -SNP_pval 1e-6 -doMaf 1 -bam bam_sorted.filelist 
+
+### Admixture ### 
+
+./angsd/misc/NGSadmix -likes Genom1.GL.beagle.gz -k3 -P 4 -O Genom1Admix -minMaf 0.05 
+
+
 
 
 
